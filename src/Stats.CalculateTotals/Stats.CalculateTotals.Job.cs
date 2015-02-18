d09c313b0686607d0c4ba519e4bce4470910b5c0
Maarten Balliauw <maarten@maartenballiauw.be>
2015-02-18 13:46:43 +0100

    Getting individual operation statistics from warehouse

--- src/Stats.CalculateTotals/Stats.CalculateTotals.Job.cs
+++ src/Stats.CalculateTotals/Stats.CalculateTotals.Job.cs
@@ -24,14 +24,21 @@ namespace Stats.CalculateTotals
                     (SELECT COUNT([Key]) FROM PackageRegistrations pr WITH (NOLOCK)
                             WHERE EXISTS (SELECT 1 FROM Packages p WITH (NOLOCK) WHERE p.PackageRegistrationKey = pr.[Key] AND p.Listed = 1)) AS UniquePackages,
                     (SELECT COUNT([Key]) FROM Packages WITH (NOLOCK) WHERE Listed = 1) AS TotalPackages,
-                    (SELECT TotalDownloadCount FROM GallerySettings WITH (NOLOCK)) AS Downloads,
-                    (SELECT -1) AS Installs";
+                    (SELECT TotalDownloadCount FROM GallerySettings WITH (NOLOCK)) AS Downloads";
+
+        private const string _sqlGetOperationsStatistics = @"SELECT Operation, SUM(DownloadCount) AS Total
+                    FROM Dimension_Operation
+                    INNER JOIN Fact_Download ON Fact_Download.Dimension_Operation_Id = Dimension_Operation.Id
+                    GROUP BY Operation
+                    ORDER BY Operation";
         
         public Job() : base(JobEventSource.Log) { }
 
         private CloudStorageAccount ContentAccount { get; set; }
 
         private SqlConnectionStringBuilder PackageDatabase { get; set; }
+
+        private SqlConnectionStringBuilder WarehouseDatabase { get; set; }
 
         public override bool Init(IDictionary<string, string> jobArgsDictionary)
         {
@@ -42,6 +49,12 @@ namespace Stats.CalculateTotals
                         JobConfigManager.GetArgument(jobArgsDictionary,
                             JobArgumentNames.PackageDatabase,
                             EnvironmentVariableKeys.SqlGallery));
+
+                WarehouseDatabase =
+                    new SqlConnectionStringBuilder(
+                        JobConfigManager.GetArgument(jobArgsDictionary,
+                            JobArgumentNames.DestinationDatabase,
+                            EnvironmentVariableKeys.SqlWarehouse));
 
                 var storageGalleryCstr = Environment.GetEnvironmentVariable(EnvironmentVariableKeys.StorageGallery);
                 if (String.IsNullOrEmpty(storageGalleryCstr))
@@ -76,8 +89,24 @@ namespace Stats.CalculateTotals
                 {
                     throw new Exception("Failed to get the Totals from the query -- no records were returned..");
                 }
+                JobEventSourceLog.FinishedQuery(totals.UniquePackages, totals.TotalPackages, totals.Downloads, totals.LastUpdateDateUtc);
 
-                JobEventSourceLog.FinishedQuery(totals.UniquePackages, totals.TotalPackages, totals.Downloads, totals.Installs, totals.LastUpdateDateUtc);
+
+                List<OperationTotal> operationTotals;
+                JobEventSourceLog.BeginningQuery(WarehouseDatabase.DataSource, WarehouseDatabase.InitialCatalog);
+                using (var connection = await WarehouseDatabase.ConnectTo())
+                {
+                    operationTotals = (await connection.QueryAsync<OperationTotal>(_sqlGetOperationsStatistics)).ToList();
+                }
+
+                if (!operationTotals.Any())
+                {
+                    throw new Exception("Failed to get the OperationTotals from the query -- no records were returned..");
+                }
+
+                JobEventSourceLog.FinishedWarehouseQuery(string.Join(", ", operationTotals.Select(t => string.Format("{0}: {1}", t.Operation, t.Total))));
+
+                totals.OperationTotals = operationTotals;
 
                 JobEventSourceLog.BeginningBlobUpload(_targetBlobName);
                 await StorageHelpers.UploadJsonBlob(blobContainer, _targetBlobName, totals.ToJsonLd());
@@ -101,7 +130,7 @@ namespace Stats.CalculateTotals
             public int UniquePackages { get; set; }
             public int TotalPackages { get; set; }
             public long Downloads { get; set; }
-            public long Installs { get; set; }
+            public List<OperationTotal> OperationTotals { get; set; }
 
             public DateTime LastUpdateDateUtc { get { return DateTime.UtcNow; } }
 
@@ -109,6 +138,12 @@ namespace Stats.CalculateTotals
             {
                 return JsonConvert.SerializeObject(this);
             }
+        }
+
+        public class OperationTotal
+        {
+            public string Operation { get; set; }
+            public long Total { get; set; }
         }
     }
 
@@ -129,29 +164,40 @@ namespace Stats.CalculateTotals
         [Event(
             eventId: 2,
             Level = EventLevel.Informational,
-            Message = "Finished querying the database. Unique Packages: {0}, Total Packages: {1}, Download Count: {2}, Installs Count: {3}, Last Updated Date UTC: {4}",
+            Message = "Finished querying the database. Unique Packages: {0}, Total Packages: {1}, Download Count: {2}, Last Updated Date UTC: {3}",
             Task = Tasks.Querying,
             Opcode = EventOpcode.Stop)]
-        public void FinishedQuery(int uniquePackages, int totalPackages, long downloadCount, long installs, DateTime lastUpdatedUtc)
+        public void FinishedQuery(int uniquePackages, int totalPackages, long downloadCount, DateTime lastUpdatedUtc)
         {
-            WriteEvent(2, uniquePackages, totalPackages, downloadCount, installs, lastUpdatedUtc);
+            WriteEvent(2, uniquePackages, totalPackages, downloadCount, lastUpdatedUtc);
         }
 
         [Event(
             eventId: 3,
             Level = EventLevel.Informational,
-            Message = "Beginning blob upload: {0}",
-            Task = Tasks.Uploading,
-            Opcode = EventOpcode.Start)]
-        public void BeginningBlobUpload(string blobName) { WriteEvent(3, blobName); }
+            Message = "Finished querying the warehouse. {0}",
+            Task = Tasks.Querying,
+            Opcode = EventOpcode.Stop)]
+        public void FinishedWarehouseQuery(string result)
+        {
+            WriteEvent(3, result);
+        }
 
         [Event(
             eventId: 4,
             Level = EventLevel.Informational,
+            Message = "Beginning blob upload: {0}",
+            Task = Tasks.Uploading,
+            Opcode = EventOpcode.Start)]
+        public void BeginningBlobUpload(string blobName) { WriteEvent(4, blobName); }
+
+        [Event(
+            eventId: 5,
+            Level = EventLevel.Informational,
             Message = "Finished blob upload",
             Task = Tasks.Uploading,
             Opcode = EventOpcode.Stop)]
-        public void FinishedBlobUpload() { WriteEvent(4); }
+        public void FinishedBlobUpload() { WriteEvent(5); }
 
         public static class Tasks
         {
