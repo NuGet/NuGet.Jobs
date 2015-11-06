f8db3da48d303864c490b96a0dc8d3081c30ae79
joyhui <joyhui@gmail.com>
2015-11-06 14:15:33 -0800

    Add sp for query download by week

---
+++ src/Stats.Warehouse/Programmability/Stored Procedures/dbo.DownloadReportByWeeks.sql
@@ -0,0 +1,24 @@
+﻿CREATE PROCEDURE [dbo].[DownloadReportByWeeks]
+	@weeks int=10
+AS
+BEGIN
+	SET NOCOUNT ON;
+
+	DECLARE @Cursor DATETIME = (SELECT MAX([Position]) FROM [dbo].[Cursors] (NOLOCK) WHERE [Name] = 'GetDirtyPackageId')
+	DECLARE @CheckTime DATETIME = getutcdate()
+
+	SELECT	D.WeekOfYearNameInYear,
+			SUM(ISNULL(Facts.[DownloadCount], 0)) 'Downloads'
+	FROM	[dbo].[Fact_Download] AS Facts (NOLOCK)
+
+	INNER JOIN	[dbo].[Dimension_Date] AS D (NOLOCK)
+	ON			D.[Id] = Facts.[Dimension_Date_Id]
+
+	WHERE	D.[Date] IS NOT NULL
+			AND DATEDIFF(week, D.[Date] , @CheckTime) <= @weeks
+			AND DATEDIFF(week, D.[Date] , @CheckTime) > 0 
+			AND Facts.[Timestamp] <= @Cursor
+
+	GROUP BY D.WeekOfYearNameInYear
+	order by D.WeekOfYearNameInYear desc
+END
