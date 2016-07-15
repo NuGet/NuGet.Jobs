5485bee8ebf8fb9ecf14fa48f10fae2afb8f92a3
shishirx34 <shishirh@microsoft.com>
2016-07-14 17:33:37 -0700

    Fixes the 'LicenseUrl' empty errors with the HandlePackageEdits job. Remove Nuget.Core and add NugetGallery.Core for packaging the edits in nuspec. Also upgrade the HandlePackageEdits' target framework to 4.5.2, required by NugetGallery.Core. Update all nuget dependencies for the framework change.
    remove unnecessary configs
    
    alignment and other minor changes
    
    Address minor fixes
    
    Adding serilog and other dependencies
    
    Fix references for new dependencies
    
    Add app insights logging
    
    Add logging using trace

--- src/HandlePackageEdits/HandlePackageEdits.Job.cs
+++ src/HandlePackageEdits/HandlePackageEdits.Job.cs
@@ -2,19 +2,18 @@
 using System.Collections.Generic;
 using System.Data.SqlClient;
 using System.Diagnostics;
-using System.Globalization;
 using System.IO;
-using System.IO.Compression;
 using System.Linq;
 using System.Security.Cryptography;
 using System.Text;
-using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using Dapper;
+using Microsoft.Extensions.Logging;
 using Microsoft.WindowsAzure.Storage;
 using Microsoft.WindowsAzure.Storage.Blob;
-using NuGet;
 using NuGet.Jobs;
+using NuGet.Services.Logging;
+using NuGetGallery.Packaging;
 
 namespace HandlePackageEdits
 {
@@ -59,34 +58,43 @@ namespace HandlePackageEdits
         protected CloudBlobContainer SourceContainer { get; set; }
         protected CloudBlobContainer BackupsContainer { get; set; }
         protected long MaxManifestSize { get; set; }
+        private ILogger _logger;
 
         public override bool Init(IDictionary<string, string> jobArgsDictionary)
         {
-            string maxManifestSizeString = JobConfigurationManager.TryGetArgument(jobArgsDictionary, JobArgumentNames.MaxManifestSize);
-            if (string.IsNullOrEmpty(maxManifestSizeString))
+            try
             {
-                MaxManifestSize = DefaultMaxAllowedManifestBytes;
+                var instrumentationKey = JobConfigurationManager.TryGetArgument(jobArgsDictionary, JobArgumentNames.InstrumentationKey);
+                ApplicationInsights.Initialize(instrumentationKey);
+
+                var loggerFactory = LoggingSetup.CreateLoggerFactory();
+                _logger = loggerFactory.CreateLogger<Job>();
+
+                var maxManifestSizeString = JobConfigurationManager.TryGetArgument(jobArgsDictionary, JobArgumentNames.MaxManifestSize);
+                MaxManifestSize = string.IsNullOrEmpty(maxManifestSizeString) ? DefaultMaxAllowedManifestBytes : Convert.ToInt64(maxManifestSizeString);
+
+                PackageDatabase = new SqlConnectionStringBuilder(
+                            JobConfigurationManager.GetArgument(jobArgsDictionary, JobArgumentNames.PackageDatabase));
+
+                Source = CloudStorageAccount.Parse(
+                                           JobConfigurationManager.GetArgument(jobArgsDictionary, JobArgumentNames.SourceStorage));
+                Backups = CloudStorageAccount.Parse(
+                                           JobConfigurationManager.GetArgument(jobArgsDictionary, JobArgumentNames.BackupStorage));
+
+                SourceContainerName = JobConfigurationManager.TryGetArgument(jobArgsDictionary, JobArgumentNames.SourceContainerName) ?? DefaultSourceContainerName;
+                BackupsContainerName = JobConfigurationManager.TryGetArgument(jobArgsDictionary, JobArgumentNames.BackupContainerName) ?? DefaultBackupContainerName;
+
+                SourceContainer = Source.CreateCloudBlobClient().GetContainerReference(SourceContainerName);
+                BackupsContainer = Backups.CreateCloudBlobClient().GetContainerReference(BackupsContainerName);
+
             }
-            else
+            catch (Exception exception)
             {
-                MaxManifestSize = Convert.ToInt64(maxManifestSizeString);
+                _logger.LogError("Failed to initalize job!", exception);
+                return false;
             }
 
-            PackageDatabase = new SqlConnectionStringBuilder(
-                        JobConfigurationManager.GetArgument(jobArgsDictionary, JobArgumentNames.PackageDatabase));
-
-            Source = CloudStorageAccount.Parse(
-                                       JobConfigurationManager.GetArgument(jobArgsDictionary, JobArgumentNames.SourceStorage));
-            Backups = CloudStorageAccount.Parse(
-                                       JobConfigurationManager.GetArgument(jobArgsDictionary, JobArgumentNames.BackupStorage));
-
-            SourceContainerName = JobConfigurationManager.TryGetArgument(jobArgsDictionary, JobArgumentNames.SourceContainerName) ?? DefaultSourceContainerName;
-            BackupsContainerName = JobConfigurationManager.TryGetArgument(jobArgsDictionary, JobArgumentNames.BackupContainerName) ?? DefaultBackupContainerName;
-
-            SourceContainer = Source.CreateCloudBlobClient().GetContainerReference(SourceContainerName);
-            BackupsContainer = Backups.CreateCloudBlobClient().GetContainerReference(BackupsContainerName);
             return true;
-
         }
 
         protected string TempDirectory;
@@ -114,6 +122,7 @@ namespace HandlePackageEdits
                         .ToList();
                 }
             }
+
             Trace.TraceInformation("Fetched {2} queued edits from {0}/{1}", PackageDatabase.DataSource, PackageDatabase.InitialCatalog, edits.Count);
 
             // Group by package and take just the most recent edit for each package
@@ -126,7 +135,7 @@ namespace HandlePackageEdits
             // Process packages
             foreach (var edit in edits)
             {
-                Trace.TraceInformation($"Editing {edit.Id} {edit.Version}");
+                _logger.LogInformation("[HandlePackageEdits] Editing {PackageId} {PackageVersion}", edit.Id, edit.Version);
                 Exception thrown = null;
                 try
                 {
@@ -136,9 +145,10 @@ namespace HandlePackageEdits
                 {
                     thrown = ex;
                 }
+
                 if (thrown != null)
                 {
-                    Trace.TraceInformation($"Error editing package {edit.Id} {edit.Version}: {thrown.ToString()}");
+                    _logger.LogError($"[HandlePackageEdits] Error editing package {edit.Id} {edit.Version}", thrown);
 
                     using (var connection = await PackageDatabase.ConnectTo())
                     {
@@ -153,14 +163,13 @@ namespace HandlePackageEdits
                                 key = edit.Key
                             });
                     }
-
                 }
-                Trace.TraceInformation($"Edited {edit.Id} {edit.Version}");
+
+                _logger.LogInformation("[HandlePackageEdits] Edited {PackageId} {PackageVersion}", edit.Id, edit.Version);
             }
+
             return true;
         }
-
-        private static readonly Regex ManifestSelector = new Regex(@"^[^/]*\.nuspec$", RegexOptions.IgnoreCase);
 
         private async Task ApplyEdit(PackageEdit edit)
         {
@@ -193,49 +202,14 @@ namespace HandlePackageEdits
                     Trace.TraceInformation($"Backed up original copy of {edit.Id} {edit.Version}");
                 }
 
-                // Load the zip file and find the manifest
+                // Update the nupkg manifest with the new metadata
                 using (var originalStream = File.Open(originalPath, FileMode.Open, FileAccess.ReadWrite))
-                using (var archive = new ZipArchive(originalStream, ZipArchiveMode.Update))
                 {
-                    // Find the nuspec
-                    var nuspecEntries = archive.Entries.Where(e => ManifestSelector.IsMatch(e.FullName)).ToArray();
-                    if (nuspecEntries.Length == 0)
-                    {
-                        throw new InvalidDataException(string.Format(
-                            CultureInfo.CurrentCulture,
-                            Strings.HandlePackageEditsJob_MissingManifest,
-                            edit.Id,
-                            edit.Version,
-                            backupBlob.Uri.AbsoluteUri));
-                    }
-                    else if (nuspecEntries.Length > 1)
-                    {
-                        throw new InvalidDataException(string.Format(
-                            CultureInfo.CurrentCulture,
-                            Strings.HandlePackageEditsJob_MultipleManifests,
-                            edit.Id,
-                            edit.Version,
-                            backupBlob.Uri.AbsoluteUri));
-                    }
-
-                    // We now have the nuspec
-                    var manifestEntry = nuspecEntries.Single();
-
-                    // Load the manifest with a constrained stream
                     Trace.TraceInformation($"Rewriting package file for {edit.Id} {edit.Version}");
-                    Manifest manifest;
-                    using (var manifestStream = manifestEntry.Open())
-                    {
-                        manifest = Manifest.ReadFrom(manifestStream, validateSchema: false);
 
-                        // Modify the manifest as per the edit
-                        edit.ApplyTo(manifest.Metadata);
+                    var editActionList = edit.GetEditsAsEnumerableAction();
+                    NupkgRewriter.RewriteNupkgManifest(originalStream, editActionList);
 
-                        // Save the manifest back
-                        manifestStream.Seek(0, SeekOrigin.Begin);
-                        manifestStream.SetLength(0);
-                        manifest.Save(manifestStream);
-                    }
                     Trace.TraceInformation($"Rewrote package file for {edit.Id} {edit.Version}");
                 }
 
@@ -360,20 +334,23 @@ namespace HandlePackageEdits
                                 DELETE FROM [PackageEdits]
                                 WHERE [PackageKey] = @PackageKey
                                 AND [Key] <= @Key
-                            " +  "COMMIT TRANSACTION",
+                            " + "COMMIT TRANSACTION",
                             parameters);
                     }
+
                     Trace.TraceInformation($"Updated package record for {edit.Id} {edit.Version}");
                 }
-                catch (Exception)
+                catch (Exception exception)
                 {
                     // Error occurred while updaing database, roll back the blob to the snapshot
                     // Can't do "await" in a catch block, but this should be pretty quick since it just starts the copy
-                    Trace.TraceInformation(
+                    _logger.LogError("[HandlePackageEdits] Failed to update database!", exception);
+                    Trace.TraceWarning(
                         $"Rolling back updated blob for {edit.Id} {edit.Version}. Copying snapshot {sourceSnapshot.Uri.AbsoluteUri} to {sourceBlob.Uri.AbsoluteUri}");
                     sourceBlob.StartCopy(sourceSnapshot);
-                    Trace.TraceInformation(
+                    Trace.TraceWarning(
                         $"Rolled back updated blob for {edit.Id} {edit.Version}. Copying snapshot {sourceSnapshot.Uri.AbsoluteUri} to {sourceBlob.Uri.AbsoluteUri}");
+
                     throw;
                 }
 
@@ -389,5 +366,5 @@ namespace HandlePackageEdits
                 }
             }
         }
-      }
+    }
 }
