3201787d9760e02050cfe1b5d0621ab49836e01e
scottbommarito <scottbom@microsoft.com>
2016-10-19 13:49:31 -0700

    update to 4.5.2 and related build fixes

--- src/NuGet.Jobs.Common/Configuration/JobConfigurationManager.cs
+++ src/NuGet.Jobs.Common/Configuration/JobConfigurationManager.cs
@@ -1,11 +1,11 @@
 ﻿// Copyright (c) .NET Foundation. All rights reserved.
 // Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
 
-using NuGet.Services.KeyVault;
 using System;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
+using NuGet.Services.Configuration;
 
 namespace NuGet.Jobs
 {
