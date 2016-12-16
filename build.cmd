49b43fff30563578f67cd4355844c3892f9a0f1d
Scott Bommarito <scottbom@microsoft.com>
2016-12-16 10:06:28 -0800

    use quotes for copy

--- build.cmd
+++ build.cmd
@@ -17,7 +17,7 @@ if not "%errorlevel%"=="0" goto failure
 REM Build VCS Callback Server
 %msbuild% src/Validation.Callback.Vcs/Validation.Callback.Vcs.csproj /p:OutputPath=obj/"%config%" /t:Package /m /v:M /fl /flp:LogFile=msbuild.log;Verbosity=Normal /nr:false
 if not "%errorlevel%"=="0" goto failure
-copy src/Validation.Callback.Vcs/obj/%config%/_PublishedWebsites/Validation.Callback.Vcs_Package/Validation.Callback.Vcs.zip src/Validation.Callback.Vcs/obj/Validation.Callback.Vcs.zip
+copy "src/Validation.Callback.Vcs/obj/%config%/_PublishedWebsites/Validation.Callback.Vcs_Package/Validation.Callback.Vcs.zip" "src/Validation.Callback.Vcs/obj/Validation.Callback.Vcs.zip"
 if not "%errorlevel%"=="0" goto failure
 
 REM Test
