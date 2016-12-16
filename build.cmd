22e9110b3dd18f08dd9cdd52d381efb806ecf234
Scott Bommarito <scottbom@microsoft.com>
2016-12-16 09:56:16 -0800

    modified build script to build vcs callback server correctly

--- build.cmd
+++ build.cmd
@@ -10,8 +10,14 @@ Powershell.exe -NoProfile -ExecutionPolicy ByPass -Command "& '%cd%\restoreNuGet
 tools\nuget.exe restore NuGet.Jobs.sln -OutputDirectory %cd%\packages -NonInteractive -source "https://api.nuget.org/v3/index.json;https://www.myget.org/F/nugetbuild/api/v3/index.json"
 if not "%errorlevel%"=="0" goto failure
 
-REM Build
+REM Build Solution
 %msbuild% NuGet.Jobs.sln /p:Configuration="%config%" /m /v:M /fl /flp:LogFile=msbuild.log;Verbosity=Normal /nr:false
+if not "%errorlevel%"=="0" goto failure
+
+REM Build VCS Callback Server
+%msbuild% src/Validation.Callback.Vcs/Validation.Callback.Vcs.csproj /p:OutputPath=obj/"%config%" /t:Package /m /v:M /fl /flp:LogFile=msbuild.log;Verbosity=Normal /nr:false
+if not "%errorlevel%"=="0" goto failure
+copy src/Validation.Callback.Vcs/obj/"%config%"/_PublishedWebsites/Validation.Callback.Vcs_Package/Validation.Callback.Vcs.zip src/Validation.Callback.Vcs/obj/Validation.Callback.Vcs.zip
 if not "%errorlevel%"=="0" goto failure
 
 REM Test
