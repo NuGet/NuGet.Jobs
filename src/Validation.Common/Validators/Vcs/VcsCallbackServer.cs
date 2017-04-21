ab534b8713a4339433093ccd8ba6b7fd6fe181fe
Andrei Grigorev <angrigor@microsoft.com>
2017-04-21 15:17:06 -0700

    Added method to truncate string. Made body completely logged in audit blob.

--- src/Validation.Common/Validators/Vcs/VcsCallbackServer.cs
+++ src/Validation.Common/Validators/Vcs/VcsCallbackServer.cs
@@ -241,13 +241,18 @@ namespace NuGet.Jobs.Validation.Common.Validators.Vcs
                             validationEntity.ValidatorCompleted(VcsValidator.ValidatorName, ValidationResult.Failed);
                             await _packageValidationTable.StoreAsync(validationEntity);
 
-                            _logger.TrackValidatorResult(VcsValidator.ValidatorName, validationId, ValidationResult.Failed.ToString(), validationEntity.PackageId, validationEntity.PackageVersion, body.Substring(0, ReasonableBodySize));
+                            _logger.TrackValidatorResult(VcsValidator.ValidatorName, 
+                                validationId, 
+                                ValidationResult.Failed.ToString(), 
+                                validationEntity.PackageId, 
+                                validationEntity.PackageVersion, 
+                                TruncateString(body, ReasonableBodySize));
                             var auditEntries = new List<PackageValidationAuditEntry>();
                             auditEntries.Add(new PackageValidationAuditEntry
                             {
                                 Timestamp = DateTimeOffset.UtcNow,
                                 ValidatorName = VcsValidator.ValidatorName,
-                                Message = "Package scan failed."
+                                Message = $"Package scan failed. Response: {body}"
                             });
 
                             if (result.ResultReasons?.ResultReason != null)
@@ -280,9 +285,27 @@ namespace NuGet.Jobs.Validation.Common.Validators.Vcs
                     _logger.LogWarning(
                         "Callback was not handled for State={State}, Result={Result}. " +
                         "Request body: {RequestBody}",
-                        result?.State, result?.Result, body.Substring(0, ReasonableBodySize));
+                        result?.State, result?.Result, TruncateString(body, ReasonableBodySize));
                 }
             }
+        }
+
+        /// <summary>
+        /// Truncates the string leaving at most specified amount of characters and adds a "(truncated)" at the end
+        /// if it removes any portion of the string
+        /// </summary>
+        /// <param name="str">String to truncate</param>
+        /// <param name="length">Max amount of characters to keep if truncated</param>
+        /// <returns>Original string if it's length was less than specified length, otherwise, first 'length' characters of the string 
+        /// with "(truncated)" appended.</returns>
+        private static string TruncateString(string str, int length)
+        {
+            if (str.Length <= length)
+            {
+                return str;
+            }
+
+            return str.Substring(0, length) + "(truncated)";
         }
     }
 }
