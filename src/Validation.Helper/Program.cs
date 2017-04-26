120a38955f80eaa596fbebf04b53b7ea332c5d55
Andrei Grigorev <angrigor@microsoft.com>
2017-04-26 15:09:17 -0700

    Validation.Helper initial commit

---
+++ src/Validation.Helper/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NuGet.Jobs.Validation.Helper
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
