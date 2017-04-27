e314563dfe3b9f0b33f7ba240cbcaded168a3bdb
Andrei Grigorev <angrigor@microsoft.com>
2017-04-26 17:56:38 -0700

    Initial argument parsing

--- src/Validation.Helper/Program.cs
+++ src/Validation.Helper/Program.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,8 +10,26 @@ namespace NuGet.Jobs.Validation.Helper
 {
     class Program
     {
-        static void Main(string[] args)
+        private static void Main(string[] args)
         {
+            if (args.Count() < 4)
+            {
+                PrintUsage();
+                return;
+            }
+
+            ISecretReaderFactory secretReaderFactory = new SecretReaderFactory();
+            IDictionary<string, string> arguments = JobConfigurationManager.GetJobArgsDictionary(args, "Validation.Helper", secretReaderFactory);
+
+            string azureStorageConnectionString = JobConfigurationManager.GetArgument(arguments, JobArgumentNames.DataStorageAccount);
+            string containerName = JobConfigurationManager.GetArgument(arguments, JobArgumentNames.ContainerName);
+        }
+
+        private static void PrintUsage()
+        {
+            Console.WriteLine("Usage: {0} " +
+                $"-{JobArgumentNames.DataStorageAccount} <Azure Blob Storage connection string> " +
+                $"-{JobArgumentNames.ContainerName} <validation job container name>", Path.GetFileName(Process.GetCurrentProcess().MainModule.FileName));
         }
     }
 }
