aa0213757c832b2a4e0fe1cdbc2c4b2a4f74ef9f
Cristina Manu <cmanu@microsoft.com>
2017-05-23 18:25:56 -0700

    version2

--- SnapshotAzureBlob/Program.cs
+++ SnapshotAzureBlob/Program.cs
@@ -1,8 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+using NuGet.Jobs;
 
 namespace SnapshotAzureBlob
 {
@@ -10,6 +9,8 @@ namespace SnapshotAzureBlob
     {
         static void Main(string[] args)
         {
+            var job = new SnapshotAzureBlobJob();
+            JobRunner.Run(job, args).Wait();
         }
     }
 }
