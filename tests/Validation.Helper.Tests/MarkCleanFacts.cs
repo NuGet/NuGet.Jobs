1bdf7d430c8968f14d747f752d58ed56c081762f
Andrei Grigorev <angrigor@microsoft.com>
2017-06-06 15:20:32 -0700

    initial tests

---
+++ tests/Validation.Helper.Tests/MarkCleanFacts.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NuGet.Jobs.Validation.Helper.Tests
+{
+    public class MarkCleanFacts
+    {
+    }
+}
