b5ec48a3480855863ec5801057f7035a494b1c04
Andrei Grigorev <angrigor@microsoft.com>
2017-06-08 16:03:00 -0700

    More using organization

--- tests/Validation.Helper.Tests/RescanFacts.cs
+++ tests/Validation.Helper.Tests/RescanFacts.cs
@@ -1,13 +1,10 @@
 ﻿// Copyright (c) .NET Foundation. All rights reserved.
 // Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
 
-using Microsoft.Extensions.Logging;
-using Moq;
 using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using Microsoft.Extensions.Logging;
+using Moq;
 using Xunit;
 
 namespace NuGet.Jobs.Validation.Helper.Tests
