7a3e9189b83db45f2983278e269175dc519a29cb
Cristina Manu <cmanu@microsoft.com>
2017-07-07 17:56:20 -0700

    remove not used scripts

--- src/NuGetCDNRedirect/Global.asax.cs
+++ src/NuGetCDNRedirect/Global.asax.cs
@@ -15,7 +15,6 @@ namespace NuGetCDNRedirect
             AreaRegistration.RegisterAllAreas();
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
-            //BundleConfig.RegisterBundles(BundleTable.Bundles);
         }
     }
 }
