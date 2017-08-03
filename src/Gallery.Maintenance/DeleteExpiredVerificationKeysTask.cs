0493c121ed546dd739452ee12b3c945c5b8a3112
scottbommarito <scottbom@microsoft.com>
2017-08-03 16:51:10 -0700

    fix deleteexpiredverificationkeystask

--- src/Gallery.Maintenance/DeleteExpiredVerificationKeysTask.cs
+++ src/Gallery.Maintenance/DeleteExpiredVerificationKeysTask.cs
@@ -59,7 +59,7 @@ DELETE FROM [dbo].[Credentials] WHERE [Key] IN ({0})";
                     using (var transaction = connection.BeginTransaction())
                     {
                         var numKeys = 0;
-                        var parameters = credentialKeys.Select(c => new SqlParameter("@Key" + numKeys++, SqlDbType.Int) { Value = c });
+                        var parameters = credentialKeys.Select(c => new SqlParameter("@Key" + numKeys++, SqlDbType.Int) { Value = c }).ToArray();
 
                         rowCount = await connection.ExecuteAsync(
                             string.Format(DeleteQuery, string.Join(",", parameters.Select(p => p.ParameterName))),
