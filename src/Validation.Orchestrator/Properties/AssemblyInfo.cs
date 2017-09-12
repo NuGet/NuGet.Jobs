6a6e20f10035b9e849cb0ac383703305ffb94136
Andrei Grigorev <angrigor@microsoft.com>
2017-09-11 18:00:33 -0700

    Added Validation.Orchestration project.

---
+++ src/Validation.Orchestrator/Properties/AssemblyInfo.cs
@@ -0,0 +1,14 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Validation.Orchestrator")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyCompany(".NET Foundation")]
+[assembly: AssemblyProduct("Validation.Orchestrator")]
+[assembly: AssemblyCopyright("Copyright © .NET Foundation 2017")]
+[assembly: ComVisible(false)]
+[assembly: Guid("e6d094fb-9068-4578-b176-116f97e7506b")]
