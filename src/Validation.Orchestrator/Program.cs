6d4a03c3bdec5b72bc524b63fbdb3e8f5c0cd93a
Andrei Grigorev <angrigor@microsoft.com>
2017-09-14 11:59:39 -0700

    Work in progress, adding logging and DI.

--- src/Validation.Orchestrator/Program.cs
+++ src/Validation.Orchestrator/Program.cs
@@ -2,10 +2,12 @@
 // Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
 
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using NuGet.Jobs;
+using NuGet.Services.Configuration;
+using NuGet.Services.Logging;
+using Serilog;
 
 namespace NuGet.Services.Validation.Orchestrator
 {
@@ -13,6 +15,38 @@ namespace NuGet.Services.Validation.Orchestrator
     {
         static void Main(string[] args)
         {
+            IServiceCollection services = new ServiceCollection();
+            ConfigureWithCommandLine(services, args);
+
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(Environment.CurrentDirectory)
+                .AddJsonFile("settings.json");
+
+            var nonInjectedConfiguration = builder.Build();
+            var secretReaderFactory = new ConfigurationRootSecretReaderFactory(nonInjectedConfiguration);
+            var configurationRoot = new SecretConfigurationReader(nonInjectedConfiguration, secretReaderFactory);
+
+            var configuration = new Configuration();
+            var configurationSection = configurationRoot.GetSection("Configuration");
+            configurationSection.Bind(configuration);
+
+            ConfigurationValidator.Validate(configuration);
         }
+
+        private static void ConfigureWithCommandLine(IServiceCollection services, string[] args)
+        {
+            var loggerConfiguration = LoggingSetup.CreateDefaultLoggerConfiguration(false);
+            loggerConfiguration.WriteTo.Console();
+
+            //services.AddTransient<ISecretReaderFactory>();
+
+            var loggerFactory = LoggingSetup.CreateLoggerFactory(loggerConfiguration);
+
+            JobConfigurationManager.GetJobArgsDictionary(loggerFactory.CreateLogger("Validation"), args, "Validation.Orchestrator", );
+
+            throw new NotImplementedException();
+        }
+
+
     }
 }
