76e7093027f86aaf6f736ba9a609c0bfea77954f
Xavier Decoster <xavier.decoster@outlook.com>
2017-11-20 22:19:24 +0100

    Fix merge conflict

--- src/NuGet.Services.Validation.Orchestrator/Job.cs
+++ src/NuGet.Services.Validation.Orchestrator/Job.cs
@@ -21,6 +21,7 @@ using NuGet.Jobs.Validation.PackageSigning.Storage;
 using NuGet.Services.Configuration;
 using NuGet.Services.KeyVault;
 using NuGet.Services.ServiceBus;
+using NuGet.Services.Validation.PackageCertificates;
 using NuGet.Services.Validation.PackageSigning;
 using NuGet.Services.Validation.Vcs;
 
