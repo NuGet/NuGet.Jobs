601ea1e3de18f8c28c9441494644d9bd416bc0a5
Xavier Decoster <xavier.decoster@outlook.com>
2017-11-23 15:36:38 +0100

    Some cosmetics

--- tests/Validation.PackageSigning.ExtractAndValidateSignature.Tests/DbAsyncQueryProviderMock.cs
+++ tests/Validation.PackageSigning.ExtractAndValidateSignature.Tests/DbAsyncQueryProviderMock.cs
@@ -1,4 +1,7 @@
-﻿using System.Data.Entity.Infrastructure;
+﻿// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+using System.Data.Entity.Infrastructure;
 using System.Linq;
 using System.Linq.Expressions;
 using System.Threading;
--- tests/Validation.PackageSigning.ExtractAndValidateSignature.Tests/Properties/AssemblyInfo.cs
+++ tests/Validation.PackageSigning.ExtractAndValidateSignature.Tests/Properties/AssemblyInfo.cs
@@ -1,36 +1,12 @@
 ﻿using System.Reflection;
-using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-// General Information about an assembly is controlled through the following
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
 [assembly: AssemblyTitle("Validation.PackageSigning.ExtractAndValidateSignature.Tests")]
-[assembly: AssemblyDescription("")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyDescription("Unit tests for ExtractAndValidateSignature job.")]
+[assembly: AssemblyCompany(".NET Foundation")]
 [assembly: AssemblyProduct("Validation.PackageSigning.ExtractAndValidateSignature.Tests")]
-[assembly: AssemblyCopyright("Copyright ©  2017")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
-
-// Setting ComVisible to false makes the types in this assembly not visible
-// to COM components.  If you need to access a type in this assembly from
-// COM, set the ComVisible attribute to true on that type.
+[assembly: AssemblyCopyright("Copyright © .NET Foundation 2017")]
 [assembly: ComVisible(false)]
-
-// The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("26435822-8938-48c9-96fd-0dccf8f7ce00")]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
