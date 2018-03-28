e9cfbeccefc54a30b330916e7b9c4a33de61a6e0
Loïc Sharma <sharma.loic@gmail.com>
2018-03-28 14:47:04 -0700

    More tests

--- src/Validation.PackageSigning.Core/Extensions/PackageSignatureExtensions.cs
+++ src/Validation.PackageSigning.Core/Extensions/PackageSignatureExtensions.cs
@@ -23,7 +23,7 @@ namespace NuGet.Services.Validation
 
             if (signature.Status == PackageSignatureStatus.Invalid)
             {
-                throw new ArgumentException($"Package signature {signature.Key} is revoked and cannot be promoted", nameof(signature));
+                throw new ArgumentException($"Package signature {signature.Key} is invalid and cannot be promoted", nameof(signature));
             }
 
             var signingTime = signature.TrustedTimestamps.Max(t => t.Value);
