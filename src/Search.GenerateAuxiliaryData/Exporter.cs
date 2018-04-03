e7e1e66455d7fb6ac2fed60068e52d8c1fccb841
Svetlana Kofman <skofman@microsoft.com>
2018-04-03 16:56:00 -0700

    Initial code

---
+++ src/Search.GenerateAuxiliaryData/Exporter.cs
@@ -0,0 +1,29 @@
+﻿// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+using System;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Logging;
+using Microsoft.WindowsAzure.Storage.Blob;
+
+namespace Search.GenerateAuxiliaryData
+{
+    // Public only to facilitate testing.
+    public abstract class Exporter
+    {
+        protected ILogger<Exporter> _logger;
+
+        public CloudBlobContainer DestinationContainer { get; }
+        public string Name { get; }
+
+        public Exporter(ILogger<Exporter> logger, CloudBlobContainer defaultDestinationContainer, string defaultName)
+        {
+            _logger = logger ?? throw new ArgumentNullException(nameof(logger));
+            DestinationContainer = defaultDestinationContainer ?? throw new ArgumentNullException(nameof(defaultDestinationContainer));
+
+            Name = defaultName;
+        }
+
+        public abstract Task ExportAsync();
+    }
+}
