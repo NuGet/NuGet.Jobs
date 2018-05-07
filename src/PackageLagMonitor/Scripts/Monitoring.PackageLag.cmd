8e52581b8e8965727389fb0b1dd768e5e86bb774
Peter Yu <ryuyu@microsoft.com>
2018-05-07 16:28:55 -0700

    Updating configs

--- src/PackageLagMonitor/Scripts/Monitoring.PackageLag.cmd
+++ src/PackageLagMonitor/Scripts/Monitoring.PackageLag.cmd
@@ -7,7 +7,7 @@ echo "Starting job - #{Jobs.monitoring.packagelag.Title}"
 
 title #{Jobs.monitoring.packagelag.Title}
 
-start /w PackageLagMonitor.exe ^
+start /w Monitoring.PackageLag.exe ^
     -Configuration #{Jobs.monitoring.packagelag.Configuration} ^
     -InstrumentationKey "#{Jobs.monitoring.packagelag.InstrumentationKey}"
 
