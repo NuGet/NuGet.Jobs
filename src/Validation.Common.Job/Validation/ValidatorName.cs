ae77f3f7c83d3e989e3abb35a2fc7e9fd9f09d57
Loïc Sharma <sharma.loic@gmail.com>
2018-05-07 09:27:14 -0700

    Fixed tests

--- src/Validation.Common.Job/Validation/ValidatorName.cs
+++ src/Validation.Common.Job/Validation/ValidatorName.cs
@@ -8,6 +8,6 @@ namespace NuGet.Jobs.Validation
         public const string Vcs = "VcsValidator";
         public const string PackageCertificate = "PackageCertificatesValidator";
         public const string PackageSignatureProcessor = "PackageSigningValidator";
-        public const string PackageSignatureValidator = "PackageSignatureValidator";
+        public const string PackageSignatureValidator = "PackageSigningValidator2";
     }
 }
