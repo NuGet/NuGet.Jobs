25c0a01c5053ed22485432fd09daec430ff444f8
Loic Sharma <sharma.loic@gmail.com>
2018-06-03 20:50:22 -0700

    WIP

--- src/Validation.PackageSigning.RepositorySign/Job.cs
+++ src/Validation.PackageSigning.RepositorySign/Job.cs
@@ -13,11 +13,12 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Options;
 using NuGet.Jobs.Configuration;
 using NuGet.Jobs.Validation;
+using NuGetGallery;
 
 namespace Validation.PackageSigning.RepositorySign
 {
-    using GalleryContext = NuGetGallery.EntitiesContext;
-    using IGalleryContext = NuGetGallery.IEntitiesContext;
+    using GalleryContext = EntitiesContext;
+    using IGalleryContext = IEntitiesContext;
 
     public class Job : JsonConfigurationJob
     {
@@ -35,29 +36,63 @@ namespace Validation.PackageSigning.RepositorySign
             var paths = new[]
             {
                 "C:\\Program Files (x86)\\Microsoft SDKs\\NuGetPackages",
-                "C:\\Program Files (x86)\\Microsoft SDKs\\NuGetPackagesFallback",
-                "%USERPROFILE%\\.dotnet\\NuGetFallbackFolder",
-                "%USERPROFILE%\\.dotnet\\NuGetFallbackFolder\\.tools"
+                // TODO: "C:\\Program Files (x86)\\Microsoft SDKs\\NuGetPackagesFallback",
+                // TODO: "C:\\Program Files (x86)\\Microsoft SDKs\\UWPNuGetPackages",
+                "%USERPROFILE%\\.nuget\\packages",
+                "%USERPROFILE%\\.nuget\\packages\\.tools"
             };
-
-            foreach (var path in paths)
-            {
-                var expandedPath = Environment.ExpandEnvironmentVariables(path);
-
-                var packageIdentifiers = Directory.GetDirectories(expandedPath).Where(p => p.StartsWith(".");
-            }
 
             using (var scope = _serviceProvider.CreateScope())
             {
+                var microsoftPackages = new HashSet<string>();
+
                 var context = scope.ServiceProvider.GetRequiredService<IGalleryContext>();
 
-                var microsoftPackages = context.PackageRegistrations
+                // Get packages owned by Microsoft
+                var packages = context.PackageRegistrations
                     .Where(r => r.Owners.Any(o => o.Username == "Microsoft"))
+                    .Select(r => r.Id.ToLowerInvariant())
                     .ToList();
 
-                
+                microsoftPackages.UnionWith(packages);
+
+                // Get packages in preinstalled paths that aren't owned by Microsoft.
+                var preinstalledPackages = new HashSet<string>();
+
+                foreach (var path in paths)
+                {
+                    var expandedPath = Environment.ExpandEnvironmentVariables(path);
+                    var packagesInPath = Directory.GetDirectories(expandedPath)
+                        .Select(d => d.Replace(expandedPath, "").Trim('\\').ToLowerInvariant())
+                        .Where(d => !d.StartsWith("."));
+
+                    preinstalledPackages.UnionWith(packagesInPath);
+                }
+
+                preinstalledPackages.ExceptWith(microsoftPackages);
+
+                var downloads = context.PackageRegistrations
+                    .Where(r => preinstalledPackages.Contains(r.Id))
+                    .Select(r => new { Id = r.Id.ToLowerInvariant(), Downloads = r.DownloadCount })
+                    .ToList();
+
+                // Get set of dependencies of Microsoft packages and pre-installed packages.
+                var microsoftOrPreinstalled = new HashSet<string>(microsoftPackages);
+                microsoftOrPreinstalled.UnionWith(preinstalledPackages);
+
+                var dependencies = context.Set<PackageDependency>()
+                    .Where(d => !microsoftOrPreinstalled.Contains(d.Id)) // TODO: test case sensitivity
+                    .Where(d => microsoftOrPreinstalled.Contains(d.Package.PackageRegistration.Id))
+                    .Select(d => d.Id.ToLowerInvariant());
+
+                var dependencyPackages = new HashSet<string>(dependencies);
+
+                // TODO: Insert microsoftPackages, preinstalledPackages, and dependencyPackages into table.
+                // For each hash set, order package ids by total downloads. For each package id, get all versions
+                // and insert them into database by descending order of versions.
             }
-            // TODO, run
+
+            await Task.Yield();
         }
 
         protected override void ConfigureJobServices(IServiceCollection services, IConfigurationRoot configurationRoot)
