bc6e6142ce32ef4ee773989f5e1f90d87161d008
Loïc Sharma <sharma.loic@gmail.com>
2018-06-04 16:46:03 -0700

    WIP

---
+++ src/Validation.PackageSigning.RepositorySign/Initializer.cs
@@ -0,0 +1,288 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Logging;
+using NuGet.Services.Validation;
+using NuGet.Versioning;
+using NuGetGallery;
+
+namespace Validation.PackageSigning.RepositorySign
+{
+    using IGalleryContext = IEntitiesContext;
+
+    public class Initializer
+    {
+        private static int BatchSize = 1000;
+
+        private static readonly string[] PreinstalledPaths = new[]
+        {
+            "C:\\Program Files (x86)\\Microsoft SDKs\\NuGetPackages",
+            // TODO: "C:\\Program Files (x86)\\Microsoft SDKs\\NuGetPackagesFallback",
+            // TODO: "C:\\Program Files (x86)\\Microsoft SDKs\\UWPNuGetPackages",
+            "%USERPROFILE%\\.nuget\\packages",
+            "%USERPROFILE%\\.nuget\\packages\\.tools"
+        };
+
+        private readonly IGalleryContext _galleryContext;
+        private readonly IValidationEntitiesContext _validationContext;
+        private readonly ILogger<Initializer> _logger;
+
+        public Initializer(
+            IGalleryContext galleryContext,
+            IValidationEntitiesContext validationContext,
+            ILogger<Initializer> logger)
+        {
+            _galleryContext = galleryContext ?? throw new ArgumentNullException(nameof(galleryContext));
+            _validationContext = validationContext ?? throw new ArgumentNullException(nameof(validationContext));
+            _logger = logger ?? throw new ArgumentNullException(nameof(logger));
+        }
+
+        public async Task InitializeAsync()
+        {
+            await ClearPackageRevalidationStateAsync();
+
+            // Find packages owned by Microsoft or preinstalled by Visual Studio.
+            var microsoftPackages = FindMicrosoftPackages();
+            var preinstalledPackages = FindPreinstalledPackages(except: microsoftPackages);
+
+            // Find the packages depended by both Microsoft and preinstalled packages.
+            var microsoftOrPreinstalledPackages = new CaseInsensitiveSet();
+
+            microsoftOrPreinstalledPackages.UnionWith(microsoftPackages);
+            microsoftOrPreinstalledPackages.UnionWith(preinstalledPackages);
+
+            var dependencyPackages = FindDependencyPackages(microsoftOrPreinstalledPackages);
+
+            // Find all other package ids.
+            var knownPackages = new CaseInsensitiveSet();
+
+            knownPackages.UnionWith(microsoftOrPreinstalledPackages);
+            knownPackages.UnionWith(dependencyPackages);
+
+            var remainingPackages = FindRemainingPackages(except: knownPackages);
+
+            // Save the packages that were found, by order of priority.
+            InitializePackageSet("Microsoft", microsoftPackages);
+            InitializePackageSet("Preinstalled", preinstalledPackages);
+            InitializePackageSet("Dependency", dependencyPackages);
+            InitializePackageSet("Remaining", remainingPackages);
+        }
+
+        private async Task ClearPackageRevalidationStateAsync()
+        {
+            _logger.LogInformation("Clearing package revalidation state, if it exists...");
+
+            // TODO
+            await Task.Yield();
+
+            _logger.LogInformation("Cleared package revalidation state");
+        }
+
+        private CaseInsensitiveSet FindMicrosoftPackages()
+        {
+            var packages = _galleryContext.PackageRegistrations
+                .Where(r => r.Owners.Any(o => o.Username == "Microsoft"))
+                .Select(r => r.Id);
+
+            return new CaseInsensitiveSet(packages);
+        }
+
+        private CaseInsensitiveSet FindPreinstalledPackages(HashSet<string> except)
+        {
+            var preinstalledPackages = new CaseInsensitiveSet();
+
+            foreach (var path in PreinstalledPaths)
+            {
+                var expandedPath = Environment.ExpandEnvironmentVariables(path);
+                var packagesInPath = Directory.GetDirectories(expandedPath)
+                    .Select(d => d.Replace(expandedPath, "").Trim('\\').ToLowerInvariant())
+                    .Where(d => !d.StartsWith("."));
+
+                preinstalledPackages.UnionWith(packagesInPath);
+            }
+
+            preinstalledPackages.ExceptWith(except);
+
+            return preinstalledPackages;
+        }
+
+        private CaseInsensitiveSet FindDependencyPackages(HashSet<string> roots)
+        {
+            var result = new CaseInsensitiveSet();
+            var next = new CaseInsensitiveSet(roots);
+            var all = new CaseInsensitiveSet(roots);
+
+            do
+            {
+                var dependencies = _galleryContext.Set<PackageDependency>()
+                    .Where(d => !all.Contains(d.Id))
+                    .Where(d => next.Contains(d.Package.PackageRegistration.Id))
+                    .Select(d => d.Id)
+                    .Distinct()
+                    .ToList();
+
+                next.Clear();
+
+                result.UnionWith(dependencies);
+                next.UnionWith(dependencies);
+                all.UnionWith(dependencies);
+            }
+            while (next.Count() > 0);
+
+            return result;
+        }
+
+        private CaseInsensitiveSet FindRemainingPackages(HashSet<string> except)
+        {
+            var packages = _galleryContext.PackageRegistrations
+                .Where(r => !except.Contains(r.Id))
+                .Select(r => r.Id)
+                .Distinct();
+
+            return new CaseInsensitiveSet(packages);
+        }
+
+        private void InitializePackageSet(string setName, CaseInsensitiveSet packageIds)
+        {
+            var batches = PartitionIntoBatchesOrderedByDownloads(setName, packageIds);
+
+            // Initialize each package id. Each version of the package will be persisted, in descending order.
+            for (var batchIndex = 0; batchIndex < batches.Count; batchIndex++)
+            {
+                _logger.LogInformation("Initializing batch {BatchIndex} of package set '{SetName}'...",
+                    batchIndex,
+                    setName);
+
+                var batch = batches[batchIndex];
+                var versions = _galleryContext.Set<Package>()
+                    .Where(p => batch.Contains(p.PackageRegistration.Id))
+                    .GroupBy(p => p.PackageRegistration.Id)
+                    .ToDictionary(
+                        g => g.Key,
+                        g => g.Select(p => p.NormalizedVersion)
+                            .Select(v => NuGetVersion.Parse(v))
+                            .OrderByDescending(v => v),
+                        StringComparer.OrdinalIgnoreCase);
+
+                foreach (var packageId in batch)
+                {
+                    foreach (var version in versions[packageId])
+                    {
+                        Console.WriteLine($"Revalidate {packageId} {version}");
+                    }
+                }
+
+                _logger.LogInformation("Initialized batch {BatchIndex} of package set '{SetName}'",
+                    batchIndex,
+                    setName);
+            }
+
+            _logger.LogInformation("Finished initializing package set '{SetName}'", setName);
+        }
+
+        private List<List<string>> PartitionIntoBatchesOrderedByDownloads(string setName, CaseInsensitiveSet packageIds)
+        {
+            _logger.LogInformation(
+                "Partitioning package set '{SetName}' into batches...",
+                setName);
+
+            var packages = FindPackageInformation(packageIds);
+            var orderedPackages = packages.OrderByDescending(p => p.Downloads).ToList();
+
+            var batches = new List<List<string>>();
+            var current = new List<string>();
+            var currentSize = 0;
+
+            foreach (var package in orderedPackages)
+            {
+                if (currentSize + package.Versions > BatchSize)
+                {
+                    batches.Add(current);
+                    current = new List<string>();
+                    currentSize = 0;
+                }
+
+                current.Add(package.Id);
+                currentSize += package.Versions;
+            }
+
+            if (current.Count > 0)
+            {
+                batches.Add(current);
+            }
+
+            _logger.LogInformation(
+                "Partitioned package set '{SetName}' into {BatchCount} batches",
+                setName,
+                batches.Count);
+
+            return batches;
+        }
+
+        private List<PackageInformation> FindPackageInformation(CaseInsensitiveSet packageIds)
+        {
+            // Batch the package ids into chunks that are manageable.
+            var packageIdList = packageIds.ToList();
+            var batches = new List<CaseInsensitiveSet>();
+            var current = new CaseInsensitiveSet();
+
+            foreach (var packageId in packageIds)
+            {
+                if (current.Count + 1 > BatchSize)
+                {
+                    batches.Add(current);
+                    current = new CaseInsensitiveSet();
+                }
+
+                current.Add(packageId);
+            }
+
+            if (current.Count > 0)
+            {
+                batches.Add(current);
+            }
+
+            // For each package batch, fetch that package's information.
+            var result = new List<PackageInformation>();
+
+            foreach (var batch in batches)
+            {
+                var packages = _galleryContext.Set<Package>()
+                    .Where(p => batch.Contains(p.PackageRegistration.Id))
+                    .GroupBy(p => p.PackageRegistration.Id)
+                    .Select(g => new PackageInformation
+                    {
+                        Id = g.Key,
+                        Downloads = g.Sum(p => p.DownloadCount),
+                        Versions = g.Count(),
+                    });
+
+                result.AddRange(packages);
+            }
+
+            return result;
+        }
+
+        private class CaseInsensitiveSet : HashSet<string>
+        {
+            public CaseInsensitiveSet()
+                : base(StringComparer.OrdinalIgnoreCase)
+            {
+            }
+
+            public CaseInsensitiveSet(IEnumerable<string> collection)
+                : base(collection, StringComparer.OrdinalIgnoreCase)
+            {
+            }
+        }
+
+        private class PackageInformation
+        {
+            public string Id { get; set; }
+            public int Downloads { get; set; }
+            public int Versions { get; set; }
+        }
+    }
+}
--- src/Validation.PackageSigning.RepositorySign/Job.cs
+++ src/Validation.PackageSigning.RepositorySign/Job.cs
@@ -10,9 +10,12 @@ using System.Threading.Tasks;
 using Autofac;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
+using NuGet.Jobs;
 using NuGet.Jobs.Configuration;
 using NuGet.Jobs.Validation;
+using NuGet.Versioning;
 using NuGetGallery;
 
 namespace Validation.PackageSigning.RepositorySign
@@ -22,16 +25,46 @@ namespace Validation.PackageSigning.RepositorySign
 
     public class Job : JsonConfigurationJob
     {
+        private const string InitializeArgumentName = "Initialize";
         private const string JobConfigurationSectionName = "RepositorySignJob";
+
+        private bool _initialize;
 
         public override void Init(IServiceContainer serviceContainer, IDictionary<string, string> jobArgsDictionary)
         {
             base.Init(serviceContainer, jobArgsDictionary);
 
-            // TODO: Get required services here
+            _initialize = JobConfigurationManager.TryGetBoolArgument(jobArgsDictionary, InitializeArgumentName);
+
+            if (_initialize && !JobConfigurationManager.TryGetBoolArgument(jobArgsDictionary, JobArgumentNames.Once))
+            {
+                throw new Exception($"Argument {JobArgumentNames.Once} is required if argument {InitializeArgumentName} is present.");
+            }
         }
 
         public override async Task Run()
+        {
+            using (var scope = _serviceProvider.CreateScope())
+            {
+                if (_initialize)
+                {
+                    Logger.LogInformation("Initializing Repository Sign job...");
+
+                    await scope.ServiceProvider
+                        .GetRequiredService<Initializer>()
+                        .InitializeAsync();
+
+                    Logger.LogInformation("Repository Sign job initialized");
+                }
+                else
+                {
+                    // TODO
+                    throw new NotImplementedException();
+                }
+            }
+        }
+
+        public async Task RunOld()
         {
             var paths = new[]
             {
@@ -44,20 +77,19 @@ namespace Validation.PackageSigning.RepositorySign
 
             using (var scope = _serviceProvider.CreateScope())
             {
-                var microsoftPackages = new HashSet<string>();
+                var microsoftPackages = new HashSet<string>(StringComparer.OrdinalIgnoreCase);
 
                 var context = scope.ServiceProvider.GetRequiredService<IGalleryContext>();
 
                 // Get packages owned by Microsoft
                 var packages = context.PackageRegistrations
                     .Where(r => r.Owners.Any(o => o.Username == "Microsoft"))
-                    .Select(r => r.Id.ToLowerInvariant())
-                    .ToList();
+                    .Select(r => r.Id);
 
                 microsoftPackages.UnionWith(packages);
 
                 // Get packages in preinstalled paths that aren't owned by Microsoft.
-                var preinstalledPackages = new HashSet<string>();
+                var preinstalledPackages = new HashSet<string>(StringComparer.OrdinalIgnoreCase);
 
                 foreach (var path in paths)
                 {
@@ -71,25 +103,88 @@ namespace Validation.PackageSigning.RepositorySign
 
                 preinstalledPackages.ExceptWith(microsoftPackages);
 
-                var downloads = context.PackageRegistrations
-                    .Where(r => preinstalledPackages.Contains(r.Id))
-                    .Select(r => new { Id = r.Id.ToLowerInvariant(), Downloads = r.DownloadCount })
-                    .ToList();
-
                 // Get set of dependencies of Microsoft packages and pre-installed packages.
-                var microsoftOrPreinstalled = new HashSet<string>(microsoftPackages);
+                var microsoftOrPreinstalled = new HashSet<string>(StringComparer.OrdinalIgnoreCase);
+
+                microsoftOrPreinstalled.UnionWith(microsoftPackages);
                 microsoftOrPreinstalled.UnionWith(preinstalledPackages);
 
                 var dependencies = context.Set<PackageDependency>()
-                    .Where(d => !microsoftOrPreinstalled.Contains(d.Id)) // TODO: test case sensitivity
+                    .Where(d => !microsoftOrPreinstalled.Contains(d.Id))
                     .Where(d => microsoftOrPreinstalled.Contains(d.Package.PackageRegistration.Id))
-                    .Select(d => d.Id.ToLowerInvariant());
+                    .Select(d => d.Id);
 
-                var dependencyPackages = new HashSet<string>(dependencies);
+                // TODO: This only gets first level of dependencies. This should iterate and get all dependencies!
+                var dependencyPackages = new HashSet<string>(dependencies, StringComparer.OrdinalIgnoreCase);
 
                 // TODO: Insert microsoftPackages, preinstalledPackages, and dependencyPackages into table.
                 // For each hash set, order package ids by total downloads. For each package id, get all versions
                 // and insert them into database by descending order of versions.
+                await InsertPackageIdSetAsync(context, microsoftPackages);
+                await InsertPackageIdSetAsync(context, preinstalledPackages);
+                await InsertPackageIdSetAsync(context, dependencyPackages);
+            }
+
+            async Task InsertPackageIdSetAsync(IGalleryContext context, HashSet<string> packageIds)
+            {
+                // Partition the set into batches of ids, ordered by their downloads.
+                // TODO: Log "Partioning package set into batches..."
+                var packages = context.Set<Package>()
+                    .Where(p => packageIds.Contains(p.PackageRegistration.Id))
+                    .GroupBy(p => p.PackageRegistration.Id)
+                    .Select(g => new
+                    {
+                        Id = g.Key,
+                        Count = g.Count(),
+                        Downloads = g.Sum(p => p.DownloadCount)
+                    })
+                    .OrderByDescending(p => p.Downloads)
+                    .ToList();
+
+                int batchSize = 1000;
+
+                var batches = new List<List<string>>();
+                var current = new List<string>();
+
+                foreach (var package in packages)
+                {
+                    if (current.Count + package.Count > batchSize)
+                    {
+                        batches.Add(current);
+                        current = new List<string>();
+                    }
+
+                    current.Add(package.Id);
+                }
+
+                if (current.Count > 0)
+                {
+                    batches.Add(current);
+                }
+
+                // TODO: Log "Partitioned package ids into X batches"
+
+                // For each batch of package ids, find the list of versions that should be persisted in the database.
+                foreach (var batch in batches)
+                {
+                    var versions = context.Set<Package>()
+                        .Where(p => batch.Contains(p.PackageRegistration.Id))
+                        .GroupBy(p => p.PackageRegistration.Id)
+                        .ToDictionary(
+                            g => g.Key,
+                            g => g.Select(p => p.NormalizedVersion)
+                                .Select(v => NuGetVersion.Parse(v))
+                                .OrderByDescending(v => v),
+                            StringComparer.OrdinalIgnoreCase);
+
+                    foreach (var packageId in batch)
+                    {
+                        foreach (var version in versions[packageId])
+                        {
+                            Console.WriteLine($"Revalidate {packageId} {version}");
+                        }
+                    }
+                }
             }
 
             await Task.Yield();
@@ -105,6 +200,8 @@ namespace Validation.PackageSigning.RepositorySign
 
                 return new GalleryContext(config.ConnectionString, readOnly: false);
             });
+
+            services.AddScoped<Initializer>();
         }
 
         protected override void ConfigureAutofacServices(ContainerBuilder containerBuilder)
