08aff49de593860f5c5b5e70e51a2eea03c727a7
Christy Henriksson <chenriks@microsoft.com>
2018-08-27 11:50:11 -0700

    Migrate job to JsonConfig

--- src/NuGet.SupportRequests.Notifications/Scripts/OnCallDailyNotification.cmd
+++ src/NuGet.SupportRequests.Notifications/Scripts/OnCallDailyNotification.cmd
@@ -8,13 +8,7 @@ title "NuGet - SupportRequests.Notifications.OnCallDailyNotification.cmd"
 
 start /w nuget.supportrequests.notifications.exe ^
 	-Task "OnCallDailyNotification" ^
-	-SourceDatabase "#{Jobs.supportrequests.notifications.SupportRequestsDatabase}" ^
-	-PagerDutyAccountName "nuget" ^
-	-PagerDutyApiKey "$$Prod-PagerDuty-ApiKey$$" ^
-	-SmtpUri "#{Jobs.supportrequests.notifications.SmtpUri}" ^
-	-VaultName "#{Deployment.Azure.KeyVault.VaultName}" ^
-	-ClientId "#{Deployment.Azure.KeyVault.ClientId}" ^
-	-CertificateThumbprint "#{Deployment.Azure.KeyVault.CertificateThumbprint}" ^
+    -Configuration "#{Jobs.supportrequests.notifications.Configuration}" ^
 	-InstrumentationKey "#{Jobs.supportrequests.notifications.InstrumentationKey}" ^
 	-verbose true ^
 	-Once
