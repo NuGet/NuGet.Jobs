3360b6b08dae6fd9e921f07ca5823e3a09479507
Scott Bommarito <scottbom@microsoft.com>
2018-10-01 16:31:43 -0700

    minor style improvements

--- src/StatusAggregator/Parse/TrafficManagerEndpointStatusIncidentParser.cs
+++ src/StatusAggregator/Parse/TrafficManagerEndpointStatusIncidentParser.cs
@@ -1,13 +1,13 @@
 ﻿// Copyright (c) .NET Foundation. All rights reserved.
 // Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
 
+using System;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
 using Microsoft.Extensions.Logging;
 using NuGet.Services.Incidents;
 using NuGet.Services.Status;
 using StatusAggregator.Factory;
-using System;
-using System.Collections.Generic;
-using System.Text.RegularExpressions;
 
 namespace StatusAggregator.Parse
 {
