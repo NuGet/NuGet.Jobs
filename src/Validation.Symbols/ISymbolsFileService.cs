4fceb92577ca7e9b91165a6094b37af517adaa19
Cristina Manu <cmanu@microsoft.com>
2018-10-16 08:51:14 -0700

    Use the validationset snupkg for validation.

--- src/Validation.Symbols/ISymbolsFileService.cs
+++ src/Validation.Symbols/ISymbolsFileService.cs
@@ -18,13 +18,15 @@ namespace Validation.Symbols
         /// <returns>The snupkg stream.</returns>
         Task<Stream> DownloadNupkgFileAsync(string packageId, string packageNormalizedVersion, CancellationToken cancellationToken);
 
-        /// <summary>
-        /// Downloads the snupkg file.
-        /// </summary>
-        /// <param name="packageId">The package id.</param>
-        /// <param name="packageNormalizedVersion">The package normalized version.</param>
-        /// <param name="cancellationToken">Cancellation token.</param>
-        /// <returns>The nupkg stream.</returns>
-        Task<Stream> DownloadSnupkgFileAsync(string packageId, string packageNormalizedVersion, CancellationToken cancellationToken);
+        ///// <summary>
+        ///// Downloads the snupkg file.
+        ///// </summary>
+        ///// <param name="packageId">The package id.</param>
+        ///// <param name="packageNormalizedVersion">The package normalized version.</param>
+        ///// <param name="cancellationToken">Cancellation token.</param>
+        ///// <returns>The nupkg stream.</returns>
+        //Task<Stream> DownloadSnupkgFileAsync(string packageId, string packageNormalizedVersion, CancellationToken cancellationToken);
+
+        Task<Stream> DownloadSnupkgFileAsync(string snupkgUri, CancellationToken cancellationToken);
     }
 }
