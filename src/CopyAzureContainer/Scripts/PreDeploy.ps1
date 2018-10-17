3fd1f298587efb4cff6092073971577d90ef5e84
Scott Bommarito <scottbom@microsoft.com>
2018-10-17 15:32:52 -0700

    add third party binaries and missing script targets

---
+++ src/CopyAzureContainer/Scripts/PreDeploy.ps1
@@ -0,0 +1,4 @@
+# This file is a placeholder--it does nothing.
+# "sign.scripts.targets", which is imported by the project from ServerCommon, requires that "nssm.exe", "Functions.ps1", "PreDeploy.ps1", and "PostDeploy.ps1" all exist.
+# Therefore, this file must exist for the build to succeed.
+# This file isn't packed into the .nupkg built because the .nuspec doesn't include it.
