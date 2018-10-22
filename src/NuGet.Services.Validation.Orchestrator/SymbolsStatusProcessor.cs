3886f0b163a7d747d8437532412261716b5be271
Cristina Manu <cmanu@microsoft.com>
2018-10-22 15:37:33 -0700

    Add tests and clean-up.

--- src/NuGet.Services.Validation.Orchestrator/SymbolsStatusProcessor.cs
+++ src/NuGet.Services.Validation.Orchestrator/SymbolsStatusProcessor.cs
@@ -1,13 +1,10 @@
 ﻿// Copyright (c) .NET Foundation. All rights reserved.
 // Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
 
-using System;
-using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.Extensions.Logging;
 using NuGet.Services.Validation.Orchestrator.Telemetry;
 using NuGetGallery;
-using NuGetGallery.Packaging;
 
 namespace NuGet.Services.Validation.Orchestrator
 {
