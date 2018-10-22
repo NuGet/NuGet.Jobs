1c1d60b392503f6bedea6f61afe3d027a10b6e39
Cristina Manu <cmanu@microsoft.com>
2018-10-22 15:41:42 -0700

    Clean-up

--- src/NuGet.Services.Validation.Orchestrator/Job.cs
+++ src/NuGet.Services.Validation.Orchestrator/Job.cs
@@ -581,7 +581,6 @@ namespace NuGet.Services.Validation.Orchestrator
                     break;
                 case ValidatingType.SymbolPackage:
                     services.AddTransient<IFileMetadataService, SymbolPackageFileMetadataService>();
-                    //services.AddTransient<IValidationFileService, ValidationSymbolFileService>();
                     services.AddTransient<IValidationFileService, ValidationFileService>();
                     break;
                 default:
@@ -596,7 +595,6 @@ namespace NuGet.Services.Validation.Orchestrator
             services.AddTransient<ICoreSymbolPackageService, CoreSymbolPackageService>();
             services.AddTransient<ICriteriaEvaluator<SymbolPackage>, SymbolCriteriaEvaluator>();
             services.AddTransient<IValidationOutcomeProcessor<SymbolPackage>, ValidationOutcomeProcessor<SymbolPackage>>();
-            //services.AddTransient<IStatusProcessor<SymbolPackage>, EntityStatusProcessor<SymbolPackage>>();
             services.AddTransient<IStatusProcessor<SymbolPackage>, SymbolsStatusProcessor>();
             services.AddTransient<IValidationSetProvider<SymbolPackage>, ValidationSetProvider<SymbolPackage>>();
             services.AddTransient<IMessageService<SymbolPackage>, SymbolsPackageMessageService>();
