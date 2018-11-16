75f11058242aaca1224b1ac3bdb63f64c4b35d82
Scott Bommarito <scottbom@microsoft.com>
2018-11-15 16:54:35 -0800

    Revert "Revert "improve comments""
    This reverts commit ae95f3791a569b5882862574647deefeb0a7a5b9.

--- src/StatusAggregator/Export/IStatusSerializer.cs
+++ src/StatusAggregator/Export/IStatusSerializer.cs
@@ -11,7 +11,7 @@ namespace StatusAggregator.Export
     public interface IStatusSerializer
     {
         /// <summary>
-        /// Serializes <paramref name="rootComponent"/> and <paramref name="recentEvents"/> and saves to storage with a time of <paramref name="lastUpdated"/>.
+        /// Serializes <paramref name="rootComponent"/> and <paramref name="recentEvents"/> and saves to storage with a last built time of <paramref name="lastBuilt"/> and a last updated time of <paramref name="lastUpdated"/>.
         /// </summary>
         Task Serialize(DateTime lastBuilt, DateTime lastUpdated, IComponent rootComponent, IEnumerable<Event> recentEvents);
     }
