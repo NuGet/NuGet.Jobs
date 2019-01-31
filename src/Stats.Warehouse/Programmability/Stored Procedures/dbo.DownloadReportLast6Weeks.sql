58cd4c49fce086afbd3957f6638c863c12832488
Andrei Grigorev <angrigor@microsoft.com>
2019-01-31 14:28:12 -0800

    Rewrote query to produce required data right away.

--- src/Stats.Warehouse/Programmability/Stored Procedures/dbo.DownloadReportLast6Weeks.sql
+++ src/Stats.Warehouse/Programmability/Stored Procedures/dbo.DownloadReportLast6Weeks.sql
@@ -19,23 +19,26 @@ BEGIN
 		AND	[Year] = @MinYear
 
 	DECLARE @Cursor DATETIME = (SELECT ISNULL(MAX([Position]), @ReportGenerationTime) FROM [dbo].[Cursors] (NOLOCK) WHERE [Name] = 'GetDirtyPackageId')
-	DECLARE @MaxDate DATE = DATEADD(DAY, 42, @MinDate)
+	DECLARE @MaxDate DATE = DATEADD(DAY, 42, @MinDate);
 
+	WITH WeekLookup AS 
+	(
+		SELECT d.[Id], dd.[WeekOfYear], dd.[Year]
+		FROM [dbo].[Dimension_Date] AS d WITH(NOLOCK)
+		CROSS APPLY
+		(
+			SELECT TOP(1) [WeekOfYear], [Year]
+			FROM [dbo].[Dimension_Date] AS d2 WITH(NOLOCK)
+			WHERE d2.[Date] <= d.[Date] AND d2.[DayOfWeek] = 1
+			ORDER BY d2.[Date] DESC
+		) AS dd
+		WHERE d.[Date] >= @MinDate AND d.[Date] < @MaxDate AND d.[Date] <= @Cursor
+	)
 	SELECT	D.[Year],
 			D.[WeekOfYear],
-			SUM(ISNULL(Facts.[DownloadCount], 0)) 'Downloads'
+			SUM(ISNULL(Facts.[DownloadCount], 0)) AS [Downloads]
 	FROM	[dbo].[Fact_Download] AS Facts (NOLOCK)
-
-	INNER JOIN	[dbo].[Dimension_Date] AS D (NOLOCK)
-	ON			D.[Id] = Facts.[Dimension_Date_Id]
-
-	WHERE	D.[Date] IS NOT NULL
-			AND ISNULL(D.[Date], CONVERT(DATE, '1900-01-01')) >= CAST(@MinDate AS DATE)
-			AND ISNULL(D.[Date], CONVERT(DATE, DATEADD(day, 1, @ReportGenerationTime))) < CAST(@ReportGenerationTime AS DATE)
-			AND Facts.[Timestamp] <= @Cursor
-			AND Facts.[Timestamp] <= @MaxDate
-
-	GROUP BY	D.[Year], D.[WeekOfYear]
-	ORDER BY	[Year], [WeekOfYear]
-
+	INNER JOIN WeekLookup AS D ON D.Id = Facts.Dimension_Date_Id
+	GROUP BY D.[Year], D.[WeekOfYear]
+	ORDER BY [Year], [WeekOfYear]
 END
