6a6d89acf412a24c07a1ff01cc4f9f444d1b8ffe
Svetlana Kofman <skofman@microsoft.com>
2019-03-25 11:27:56 -0700

    minor

--- tests/Tests.Stats.ImportAzureCdnStatistics/PackageStatisticsParserFacts.cs
+++ tests/Tests.Stats.ImportAzureCdnStatistics/PackageStatisticsParserFacts.cs
@@ -2,7 +2,6 @@
 // Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
 
 using Microsoft.Extensions.Logging;
-using Newtonsoft.Json.Linq;
 using Stats.AzureCdnLogs.Common;
 using Stats.ImportAzureCdnStatistics;
 using System;
