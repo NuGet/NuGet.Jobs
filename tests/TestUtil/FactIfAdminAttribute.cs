a60df6c58808cfe388c15707656cccf9df761692
Andrei Grigorev <angrigor@microsoft.com>
2019-06-13 15:16:29 -0700

    Skipping admin-only tests when run as non-admin gracefullly instead of failing them.

---
+++ tests/TestUtil/FactIfAdminAttribute.cs
@@ -0,0 +1,20 @@
+﻿// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+using System;
+using Xunit;
+
+namespace TestUtil
+{
+    [AttributeUsage(AttributeTargets.Method, AllowMultiple = false)]
+    public class FactIfAdminAttribute : FactAttribute
+    {
+        public FactIfAdminAttribute()
+        {
+            if (!UserHelper.IsAdmin())
+            {
+                Skip = "Test will not run unless executed as Administrator";
+            }
+        }
+    }
+}
---
+++ tests/TestUtil/UserHelper.cs
@@ -0,0 +1,18 @@
+﻿// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+using System.Security.Principal;
+
+namespace TestUtil
+{
+    public static class UserHelper
+    {
+        public static bool IsAdmin()
+        {
+            var identity = WindowsIdentity.GetCurrent();
+            var principal = new WindowsPrincipal(identity);
+            bool isAdmin = principal.IsInRole(WindowsBuiltInRole.Administrator);
+            return isAdmin;
+        }
+    }
+}
