e2e093c9cd8a49b2411a53b8890e68f811f94d92
Riad Mohamed Gahlouz <t-mogah@microsoft.com>
2019-06-18 12:24:50 -0700

    [GH Idx] Add Filters class

---
+++ src/NuGet.Jobs.GitHubIndexer/Filters.cs
@@ -0,0 +1,59 @@
+﻿// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+using System;
+using System.IO;
+
+namespace NuGet.Jobs.GitHubIndexer
+{
+    public static class Filters
+    {
+        public enum ConfigFileType
+        {
+            PROJ,
+            PROPS,
+            TARGETS,
+            PKG_CONFIG,
+            NONE
+        }
+
+        /// <summary>
+        /// Returns the type of configuration file the filename points to. If the file type is not a valid config file,
+        /// a ConfigFileType.NONE value is returned.
+        /// </summary>
+        /// <param name="fileName">The name of the file to look at</param>
+        /// <exception cref="ArgumentNullException">Thrown then the filename is null</exception>
+        /// <exception cref="ArgumentException">Thrown then the filename is invalid</exception>
+        /// <returns>An enum indicating the file type</returns>
+        public static ConfigFileType GetConfigFileType(string fileName)
+        {
+            if (fileName == null)
+            {
+                throw new ArgumentNullException(nameof(fileName));
+            }
+
+            if (string.Equals(fileName, "packages.config", StringComparison.OrdinalIgnoreCase))
+            {
+                return ConfigFileType.PKG_CONFIG;
+            }
+
+            var ext = Path.GetExtension(fileName).ToLower();
+            var cfgFileType = ConfigFileType.NONE;
+
+            if (ext.EndsWith("proj"))
+            {
+                cfgFileType = ConfigFileType.PROJ;
+            }
+            else if (ext.EndsWith(".props"))
+            {
+                cfgFileType = ConfigFileType.PROPS;
+            }
+            else if (ext.EndsWith(".targets"))
+            {
+                cfgFileType = ConfigFileType.TARGETS;
+            }
+
+            return cfgFileType;
+        }
+    }
+}
---
+++ tests/NuGet.Jobs.GitHubIndexer.Tests/FiltersFacts.cs
@@ -0,0 +1,53 @@
+﻿// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+using System;
+using Xunit;
+
+namespace NuGet.Jobs.GitHubIndexer.Tests
+{
+    public class FiltersFacts
+    {
+        public class TheGetConfigFileTypeFunction
+        {
+            [Fact]
+            public void NullFileName()
+            {
+                Assert.Throws<ArgumentNullException>(() => { Filters.GetConfigFileType(null); });
+            }
+
+            [Fact]
+            public void InvalidFileName()
+            {
+                Assert.Throws<ArgumentException>(() => { Filters.GetConfigFileType("\0"); });
+            }
+
+            [Fact]
+            public void UnklnownConfigFileType()
+            {
+                Assert.Equal(Filters.ConfigFileType.NONE, Filters.GetConfigFileType("File.riad"));
+                Assert.Equal(Filters.ConfigFileType.NONE, Filters.GetConfigFileType("File.config"));
+            }
+
+            [Fact]
+            public void KnownConfigFileTypes()
+            {
+                Assert.Equal(Filters.ConfigFileType.PROJ, Filters.GetConfigFileType("File.proj"));
+                Assert.Equal(Filters.ConfigFileType.PROJ, Filters.GetConfigFileType("File.csproj"));
+                Assert.Equal(Filters.ConfigFileType.PROPS, Filters.GetConfigFileType("File.props"));
+                Assert.Equal(Filters.ConfigFileType.TARGETS, Filters.GetConfigFileType("File.targets"));
+                Assert.Equal(Filters.ConfigFileType.PKG_CONFIG, Filters.GetConfigFileType("packages.config"));
+            }
+
+            [Fact]
+            public void KnownConfigFileTypesCaseInsensitive()
+            {
+                Assert.Equal(Filters.ConfigFileType.PROJ, Filters.GetConfigFileType("File.pRoj"));
+                Assert.Equal(Filters.ConfigFileType.PROJ, Filters.GetConfigFileType("File.CsProj"));
+                Assert.Equal(Filters.ConfigFileType.PROPS, Filters.GetConfigFileType("File.proPS"));
+                Assert.Equal(Filters.ConfigFileType.TARGETS, Filters.GetConfigFileType("File.tarGETs"));
+                Assert.Equal(Filters.ConfigFileType.PKG_CONFIG, Filters.GetConfigFileType("paCkAges.cONfiG"));
+            }
+        }
+    }
+}
