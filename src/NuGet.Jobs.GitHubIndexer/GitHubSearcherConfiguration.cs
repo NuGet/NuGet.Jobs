6096aa1019d12e35d7e20792f58b80b0a3770f26
Riad Mohamed Gahlouz <t-mogah@microsoft.com>
2019-06-24 14:15:03 -0700

    [GH Idx] Add documentation to the configuration

--- src/NuGet.Jobs.GitHubIndexer/GitHubSearcherConfiguration.cs
+++ src/NuGet.Jobs.GitHubIndexer/GitHubSearcherConfiguration.cs
@@ -5,10 +5,19 @@ namespace NuGet.Jobs.GitHubIndexer
 {
     public class GitHubSearcherConfiguration
     {
+        /// <summary>
+        /// Minimum number of stars that a GitHub Repo needs to have to be included in the indexing
+        /// </summary>
         public int MinStars { get; set; } = 100;
 
+        /// <summary>
+        /// The number of results that would be shown per page. This is currently limited to 100 (limit verified on 6/24/2019)
+        /// </summary>
         public int ResultsPerPage { get; set; } = 100;
 
-        public int MaxGitHubResultPerQuery { get; set; } = 1000;
+        /// <summary>
+        /// The limit of results that a single search query can show. This is currently limited to 1000 (limit verified on 6/24/2019)
+        /// </summary>
+        public int MaxGitHubResultsPerQuery { get; set; } = 1000;
     }
 }
