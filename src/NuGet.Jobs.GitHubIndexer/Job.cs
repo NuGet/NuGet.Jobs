07a2e4518e4c91b1dc12117e698c29167aedd50c
Riad Mohamed Gahlouz <t-mogah@microsoft.com>
2019-06-24 16:14:45 -0700

    [GH Idx] Change UserAgent to use assembly name and version

--- src/NuGet.Jobs.GitHubIndexer/Job.cs
+++ src/NuGet.Jobs.GitHubIndexer/Job.cs
@@ -2,6 +2,7 @@
 // Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
 
 using System.IO;
+using System.Reflection;
 using System.Threading.Tasks;
 using Autofac;
 using Microsoft.Extensions.Configuration;
@@ -13,7 +14,6 @@ namespace NuGet.Jobs.GitHubIndexer
 {
     public class Job : JsonConfigurationJob
     {
-        private const string GitHubIndexerUserAgent = "NuGet-NuGet.Jobs-GitHubIndexer";
         private const string GitHubSearcherConfigurationSectionName = "GitHubSearcher";
 
         public override async Task Run()
@@ -26,8 +26,12 @@ namespace NuGet.Jobs.GitHubIndexer
 
         protected override void ConfigureJobServices(IServiceCollection services, IConfigurationRoot configurationRoot)
         {
+            var assembly = Assembly.GetEntryAssembly();
+            var assemblyName = assembly.GetName().Name;
+            var assemblyVersion = assembly.GetCustomAttribute<AssemblyInformationalVersionAttribute>()?.InformationalVersion ?? "0.0.0";
+
             services.AddTransient<IGitRepoSearcher, GitHubSearcher>();
-            services.AddSingleton<IGitHubClient>(provider => new GitHubClient(new ProductHeaderValue(GitHubIndexerUserAgent)));
+            services.AddSingleton<IGitHubClient>(provider => new GitHubClient(new ProductHeaderValue(assemblyName, assemblyVersion)));
             services.AddSingleton<IGitHubSearchWrapper, GitHubSearchWrapper>();
 
             services.Configure<GitHubSearcherConfiguration>(configurationRoot.GetSection(GitHubSearcherConfigurationSectionName));
