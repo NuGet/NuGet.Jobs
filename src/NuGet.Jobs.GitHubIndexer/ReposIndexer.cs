71af8f413b71f7f9f5457decc07ed1b0c70ca2cf
Riad Mohamed Gahlouz <t-mogah@microsoft.com>
2019-07-18 16:20:41 -0700

    [GH Idx] workdir --> work

--- src/NuGet.Jobs.GitHubIndexer/ReposIndexer.cs
+++ src/NuGet.Jobs.GitHubIndexer/ReposIndexer.cs
@@ -17,7 +17,7 @@ namespace NuGet.Jobs.GitHubIndexer
 {
     public class ReposIndexer
     {
-        private const string WorkingDirectory = "workdir";
+        private const string WorkingDirectory = "work";
 
         private static readonly string GitHubUsageFilePath = Path.Combine(WorkingDirectory, "GitHubUsage.v1.json");
         public static readonly string RepositoriesDirectory = Path.Combine(WorkingDirectory, "repos");
