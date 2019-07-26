cc3e7d7d61aa902c8ade1d553e8a9d6414000497
Andrei Grigorev <angrigor@microsoft.com>
2019-07-26 16:54:47 -0700

    Revert "Reverting some of the previous reverts"
    This reverts commit 0d16837e483ae74d1b7b05088b32ff1aa5e73f7f.

--- tests/NuGet.Services.Validation.Orchestrator.Tests/ValidationOutcomeProcessorFacts.cs
+++ tests/NuGet.Services.Validation.Orchestrator.Tests/ValidationOutcomeProcessorFacts.cs
@@ -451,6 +451,9 @@ namespace NuGet.Services.Validation.Orchestrator.Tests
                     Times.Never);
             }
 
+            PackageFileServiceMock.Verify(
+                x => x.DeletePackageForValidationSetAsync(ValidationSet),
+                Times.Never);
             TelemetryServiceMock
                 .Verify(ts => ts.TrackTotalValidationDuration(It.IsAny<string>(), It.IsAny<string>(), It.IsAny<Guid>(), It.IsAny<TimeSpan>(), It.IsAny<bool>()), Times.Once());
             Assert.InRange(duration, before - ValidationSet.Created, after - ValidationSet.Created);
