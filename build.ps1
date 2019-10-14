ae02d8c8f431688407ef82f9da309d65a5288d4c
Scott Bommarito <scottbom@microsoft.com>
2019-10-14 13:51:49 -0700

    add GitHubVulnerability2DB

--- build.ps1
+++ build.ps1
@@ -102,7 +102,8 @@ Invoke-BuildStep 'Set version metadata in AssemblyInfo.cs' { `
             "$PSScriptRoot\src\Validation.Symbols.Core\Properties\AssemblyInfo.g.cs",
             "$PSScriptRoot\src\Monitoring.RebootSearchInstance\Properties\AssemblyInfo.g.cs",
             "$PSScriptRoot\src\Stats.CDNLogsSanitizer\Properties\AssemblyInfo.g.cs",
-            "$PSScriptRoot\src\NuGet.Jobs.GitHubIndexer\Properties\AssemblyInfo.g.cs"
+            "$PSScriptRoot\src\NuGet.Jobs.GitHubIndexer\Properties\AssemblyInfo.g.cs",
+            "$PSScriptRoot\src\GitHubVulnerability2DB\Properties\AssemblyInfo.g.cs"
             
         $versionMetadata | ForEach-Object {
             Set-VersionInfo -Path $_ -Version $SimpleVersion -Branch $Branch -Commit $CommitSHA
@@ -169,7 +170,8 @@ Invoke-BuildStep 'Creating artifacts' {
             "src/Validation.Symbols.Core/Validation.Symbols.Core.csproj", `
             "src/Validation.Symbols/Validation.Symbols.Job.csproj", `
             "src/Stats.CDNLogsSanitizer/Stats.CDNLogsSanitizer.csproj", `
-            "src/NuGet.Jobs.GitHubIndexer/NuGet.Jobs.GitHubIndexer.nuspec"
+            "src/NuGet.Jobs.GitHubIndexer/NuGet.Jobs.GitHubIndexer.nuspec", `
+            "src/GitHubVulnerability2DB/GitHubVulnerability2DB.nuspec"
 
         Foreach ($Project in $NuspecProjects) {
             New-Package (Join-Path $PSScriptRoot "$Project") -Configuration $Configuration -BuildNumber $BuildNumber -Version $SemanticVersion -Branch $Branch -MSBuildVersion "$msBuildVersion"
