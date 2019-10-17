a886cbf7a4cfea334196856198e315f9d7054015
Scott Bommarito <scottbom@microsoft.com>
2019-10-17 16:34:58 -0700

    configure gallery DI

---
+++ src/GitHubVulnerability2Db/Gallery/ThrowingAuditingService.cs
@@ -0,0 +1,16 @@
+﻿// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+using System.Threading.Tasks;
+using NuGetGallery.Auditing;
+
+namespace GitHubVulnerability2Db.Gallery
+{
+    public class ThrowingAuditingService : IAuditingService
+    {
+        public Task SaveAuditRecordAsync(AuditRecord record)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
