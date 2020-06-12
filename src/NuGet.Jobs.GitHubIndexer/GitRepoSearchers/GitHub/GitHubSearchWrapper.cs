5aca7ab404cc5e294c5f760ba466c1071e61efec
Riad Mohamed Gahlouz <t-mog@microsoft.com>
2020-06-12 16:26:03 -0400

    Add cast insensitive header fix for GH Indexer

--- src/NuGet.Jobs.GitHubIndexer/GitRepoSearchers/GitHub/GitHubSearchWrapper.cs
+++ src/NuGet.Jobs.GitHubIndexer/GitRepoSearchers/GitHub/GitHubSearchWrapper.cs
@@ -1,7 +1,9 @@
-﻿// Copyright (c) .NET Foundation. All rights reserved.
+// Copyright (c) .NET Foundation. All rights reserved.
 // Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
 
 using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Globalization;
 using System.IO;
 using System.Linq;
@@ -28,7 +30,11 @@ namespace NuGet.Jobs.GitHubIndexer
         public async Task<GitHubSearchApiResponse> GetResponse(SearchRepositoriesRequest request)
         {
             var apiResponse = await _client.Connection.Get<SearchRepositoryResult>(ApiUrls.SearchRepositories(), request.Parameters, null);
-            if (!apiResponse.HttpResponse.Headers.TryGetValue("Date", out var ghStrDate)
+
+            // According to RFC 2616, Http headers are case-insensitive. We should treat them as such.
+            var caseInsensitiveHeaders = apiResponse.HttpResponse.Headers.AsEnumerable().ToDictionary(x => x.Key, x => x.Value, StringComparer.OrdinalIgnoreCase);
+
+            if (!caseInsensitiveHeaders.TryGetValue("Date", out var ghStrDate)
                 || !DateTimeOffset.TryParseExact(ghStrDate.Replace("GMT", "+0"), "ddd',' dd MMM yyyy HH:mm:ss z", CultureInfo.InvariantCulture, DateTimeStyles.None, out var ghTime))
             {
                 throw new InvalidDataException("Date is missing, has a wrong format or is not in GMT timezone");
