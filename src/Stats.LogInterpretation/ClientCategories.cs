e22a3f163038b6f8fe1bb96d6ccea89ae1cb7db0
Shishir Horane <shishirh@microsoft.com>
2020-11-25 15:15:18 -0800

    Add client name to category translation support

---
+++ src/Stats.LogInterpretation/ClientCategories.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Stats.LogInterpretation
+{
+    public static class ClientCategories
+    {
+        public const string NuGet = "NuGet";
+        public const string WebMatrix = "WebMatrix";
+        public const string NuGetPackageExplorer = "NuGet Package Explorer";
+        public const string Script = "Script";
+        public const string Crawler = "Crawler";
+        public const string Mobile = "Mobile";
+        public const string Browser = "Browser";
+        public const string Unknown = "Unknown";
+    }
+}
---
+++ src/Stats.LogInterpretation/ClientNames.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Stats.LogInterpretation
+{
+    public static class ClientNames
+    {
+        public static IList<string> NuGet = new List<string>()
+        {
+            "NuGet Cross-Platform Command Line",
+            "NuGet Client V3",
+
+            //-- VS NuGet 4.6+
+            "NuGet VS VSIX",
+
+            //-- VS NuGet 2.8+
+            "NuGet VS PowerShell Console",
+            "NuGet VS Packages Dialog - Solution",
+            "NuGet VS Packages Dialog",
+            "NuGet Shim",
+
+            //-- VS NuGet (pre-2.8)
+            "NuGet Add Package Dialog",
+            "NuGet Command Line",
+            "NuGet Package Manager Console",
+            "NuGet Visual Studio Extension",
+            "Package-Installer",
+
+            //-- dotnet restore / msbuild /t:Restore
+            "NuGet MSBuild Task",
+            "NuGet .NET Core MSBuild Task",
+            "NuGet Desktop MSBuild Task",
+        };
+
+        public static IList<string> WebMatrix = new List<string>()
+        {
+            "WebMatrix"
+        };
+
+        public static IList<string> NuGetPackageExplorer = new List<string>()
+        {
+            "NuGet Package Explorer Metro",
+            "NuGet Package Explorer"
+        };
+
+        public static IList<string> Script = new List<string>()
+        {
+            "Powershell",
+            "curl",
+            "Wget",
+            "Java"
+        };
+
+        public static IList<string> Crawler = new List<string>()
+        {
+            "Bot",
+            "bot",
+            "Slurp",
+            "BingPreview",
+            "crawler",
+            "sniffer",
+            "spider"
+        };
+
+        public static IList<string> Mobile = new List<string>()
+        {
+            "Mobile",
+            "Android",
+            "Kindle",
+            "BlackBerry",
+            "Openwave",
+            "NetFront",
+            "CFNetwork",
+            "iLunascape"
+        };
+
+        public static IList<string> Browser = new List<string>()
+        {
+            "Mozilla",
+            "Firefox",
+            "Opera",
+            "Chrome",
+            "Chromium",
+            "Internet Explorer",
+            "Browser",
+            "Safari",
+            "Sogou Explorer",
+            "Maxthon",
+            "SeaMonkey",
+            "Iceweasel",
+            "Sleipnir",
+            "Konqueror",
+            "Lynx",
+            "Galeon",
+            "Epiphany",
+            "Lunascape"
+        };
+
+        public static IList<string> AbsoluteBrowserNames = new List<string>()
+        {
+            "IE",
+            "Iron"
+        };
+
+        public static IList<string> Unknown = new List<string>()
+        {
+            "PhantomJS",
+            "WebKit Nightly",
+            "Python Requests",
+            "Jasmine",
+            "Java",
+            "AppleMail",
+            "NuGet Test Client"
+        };
+    }
+}
