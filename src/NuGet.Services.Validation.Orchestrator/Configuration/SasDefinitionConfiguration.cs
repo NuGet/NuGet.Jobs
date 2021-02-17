c6bd14eed0f41a717e88f6634133b37c8260db5b
Daniel Jacinto <dajacint@microsoft.com>
2021-02-16 20:51:58 -0800

    SasDefinitionConfiguration description.

--- src/NuGet.Services.Validation.Orchestrator/Configuration/SasDefinitionConfiguration.cs
+++ src/NuGet.Services.Validation.Orchestrator/Configuration/SasDefinitionConfiguration.cs
@@ -5,8 +5,19 @@ namespace NuGet.Services.Validation.Orchestrator
 {
     public class SasDefinitionConfiguration
     {
+        /// <summary>
+        /// Sas definition name already stored on key vault that is used by the <see cref="PackageStatusProcessor"/>.
+        /// </summary>
         public string PackageStatusProcessorSasDefinition { get; set; }
+
+        /// <summary>
+        /// Sas definition name already stored on key vault that is used by the <see cref="ValidationSetProvider{T}"/>.
+        /// </summary>
         public string ValidationSetProviderSasDefinition { get; set; }
+
+        /// <summary>
+        /// Sas definition name already stored on key vault that is used by the <see cref="ValidationSetProcessor"/>.
+        /// </summary>
         public string ValidationSetProcessorSasDefinition { get; set; }
     }
 }
