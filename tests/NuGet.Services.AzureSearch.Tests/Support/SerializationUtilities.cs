05a620b305737ae8ed151dc742a4fc97eddf1e9b
Joel Verhagen <jver@microsoft.com>
2022-02-17 15:49:30 -0800

    Update tests/NuGet.Services.AzureSearch.Tests/Support/SerializationUtilities.cs
    Co-authored-by: Loïc Sharma <737941+loic-sharma@users.noreply.github.com>

--- tests/NuGet.Services.AzureSearch.Tests/Support/SerializationUtilities.cs
+++ tests/NuGet.Services.AzureSearch.Tests/Support/SerializationUtilities.cs
@@ -23,7 +23,7 @@ namespace NuGet.Services.AzureSearch.Support
             using (var testHandler = new TestHttpClientHandler())
             {
                 var indexClient = new SearchClient(
-                   new Uri("https://unit-test-service/"),
+                   new Uri("https://unit-test-service.test/"),
                    "unit-test-index",
                    new AzureKeyCredential("unit-test-api-key"),
                    new SearchClientOptions
