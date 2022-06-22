f0748f3a346367a26693426757cc5d29eb05058a
Joel Verhagen <jver@microsoft.com>
2022-06-22 10:14:29 -0700

    Fix commit

--- build.ps1
+++ build.ps1
@@ -9,7 +9,7 @@ param (
     [string]$SemanticVersion = '1.0.0-zlocal',
     [string]$Branch = 'zlocal',
     [string]$CommitSHA,
-    [string]$BuildBranchCommit = 'b5622de52178a270b3927fa5a15541061ad77edb'
+    [string]$BuildBranchCommit = 'c95c1d7cddf2199cc76bdfa0bd326defbc2d3f83'
 )
 
 # For TeamCity - If any issue occurs, this script fails the build. - By default, TeamCity returns an exit code of 0 for all powershell scripts, even if they fail
